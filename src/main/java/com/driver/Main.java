0897ffa540da0eaf6268aff4d1df1f6e253b42d8
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-07-20 13:41:58 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
-public class Main {
+public class A {
+
+  public String meth(){
+
+    return "Invoking";
+  }
   
+  
+}
+public class B extends A {
+
+  public String meth(){
+
+    return "Method is Overriden in extended";
+
+    public static void main(String args[]){
+
+      B b = new B();
+      System.out.print(b.meth);
+    }
 }
